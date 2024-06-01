0d4765ce08578a057ec316cee6d9d79502f767fd
DIPAJOSHI <joshideepa988@gmail.com>
2024-06-01 22:53:12 +0545

    initial commit

---
+++ Server/Routes/router.js
@@ -0,0 +1,33 @@
+// const express = require("express");
+// const router = new express.Router();
+// const usersController = require("../Controllers/usersControllers");
+
+// const upload = require("../multerconfig/storageConfig");
+
+// // Define your routes here
+// // Example: router.get('/users', usersController.getUsers);
+
+// module.exports = router;
+
+
+
+
+
+
+
+
+
+// const express = require("express");
+// const router = new express.Router();
+// const controllers = require("../Controllers/usersControllers");
+// const usersController = require('../Controllers/usersControllers');
+
+// const upload = require("../multerconfig/storageConfig");
+
+
+
+// //routes
+
+
+
+// module.exports = router

36fc2a2741a8084227ba4599fd9d9e969d8c220c
DIPAJOSHI <joshideepa988@gmail.com>
2024-06-01 22:38:39 +0545

    File added

---
+++ Server/Controllers/userControllers.js
@@ -0,0 +1,14 @@
+// const users = require("../models/usersSchema");
+
+// // Example content for usersControllers.js
+// exports.getUsers = (req, res) => {
+//     res.send('Get Users');
+// };
+
+
+
+// exports.userpost = async(req,res)=>{
+
+// }
+
+// npm i multer

37d1bef51996c4ff01126c3375b2af6460b4222a
DIPAJOSHI <joshideepa988@gmail.com>
2024-06-01 09:30:45 +0545

    files added to components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Home from './Pages/Home/Home';
+import About_Us from './Pages/About_Us/About_Us';
+import Article from './Pages/Article/Article';
+import AI from './Pages/AI/AI';
+import Chat from './Pages/Chat/Chat';
+import {Routes,Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route/>
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/Pages/About_Us/About_Us.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About_Us() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default About_Us

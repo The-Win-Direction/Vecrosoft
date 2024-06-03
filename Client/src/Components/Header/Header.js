de738d1b8d59935df704c5f299756be50660dc4e
DIPAJOSHI <joshideepa988@gmail.com>
2024-06-03 08:49:26 +0545

    navbar and header structure added

--- Client/src/Components/Header/Header.js
+++ Client/src/Components/Header/Header.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
-  return (
-    <div>
-      Header
-    </div>
-  )
-}
+    const location = useLocation();
+    const hideNav = location.pathname === '/login' || location.pathname === '/signup';
 
-export default Header
+    return (
+        <header className="header">
+            <div className="header-top">
+                <div className="logo-box">
+                    <img src="/src/Assets/Images/logo.png" alt="Vecrosoft Logo1" />
+                    <h1>Vecrosoft</h1>
+                </div>
+            </div>
+            {!hideNav && (
+                <div className="navbar">
+                    <nav>
+                        <Link to="/">Home</Link>
+                        <Link to="/About_Us">About Us</Link>
+                        <Link to="/Ai">AI</Link>
+                        <Link to="/Chat">Chat</Link>
+                        <Link to="/Article">Articles</Link>
+                    </nav>
+                </div>
+            )}
+        </header>
+    );
+};
 
+export default Header;

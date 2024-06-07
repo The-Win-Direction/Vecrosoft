2d16bb931337b7e37ef206dcffc78418c7fa36d8
DIPAJOSHI <joshideepa988@gmail.com>
2024-06-07 23:46:52 +0545

    simple structure of ai page

--- Client/src/Pages/AI/AI.js
+++ Client/src/Pages/AI/AI.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './ai.css'
 
 function AI() {
   return (
-    <div>
-      AI
+    <div class="container">
+     <h1>Plant Disease Classifier With AI</h1>
+     <h2>Made By Vecrosoft</h2>
+     <h3>Choose an image</h3>
+     <div class="image-drag">
+      <p>Drag and drop image here</p>
+      <button>Browse image</button>
+     </div>
+     <button>Predict</button>
     </div>
   )
 }

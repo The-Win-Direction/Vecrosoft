e807bbaece780a9cd705c43a086e75da89e7996d
DIPAJOSHI <joshideepa988@gmail.com>
2024-06-07 22:12:46 +0545

    rendering of ans when clicking on ques

--- Client/src/Pages/AboutUs/AboutUs.js
+++ Client/src/Pages/AboutUs/AboutUs.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './about_us.css';
 import about from '../../Assets/Images/about.png';
 
-
 function AboutUs() {
+  const [visibleAnswers, setVisibleAnswers] = useState({});
+
+  const handleQuestionClick = (question) => {
+    setVisibleAnswers((prevVisibleAnswers) => ({
+      ...prevVisibleAnswers,
+      [question]: !prevVisibleAnswers[question]
+    }));
+  };
 
   return (
     <div className="about-us">
@@ -11,18 +18,38 @@ function AboutUs() {
         <h1>Our Story</h1>
         <img src={about} alt="About Us" />
         <div className="questions">
-        
-        <ul>
-        <hr></hr>
-          <li> Who we are    </li>  <hr></hr>
-          <li>  What we do </li> <hr></hr>
-          <li>How to help</li>  <hr></hr>
-          <li>Where we work</li>  <hr></hr>
-        </ul>
+          <ul>
+            <hr />
+            <li onClick={() => handleQuestionClick('who')}>
+              Who we are?
+              {visibleAnswers['who'] && (
+                <p className="answer">We are a company dedicated to ...</p>
+              )}
+            </li>
+            <hr />
+            <li onClick={() => handleQuestionClick('what')}>
+              What we do?
+              {visibleAnswers['what'] && (
+                <p className="answer">We specialize in ...</p>
+              )}
+            </li>
+            <hr />
+            <li onClick={() => handleQuestionClick('how')}>
+              How to help?
+              {visibleAnswers['how'] && (
+                <p className="answer">You can help by ...</p>
+              )}
+            </li>
+            <hr />
+            <li onClick={() => handleQuestionClick('where')}>
+              Where we work?
+              {visibleAnswers['where'] && (
+                <p className="answer">Our offices are located in ...</p>
+              )}
+            </li>
+            <hr />
+          </ul>
         </div>
-
-  
-
       </div>
     </div>
   );

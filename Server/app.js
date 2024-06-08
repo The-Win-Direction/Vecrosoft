46440149f52ba01fdaeed6ea1434c204d5a5149b
Dipak Raj Giri <giridipak743@gmail.com>
2024-06-08 08:55:24 +0545

    login

--- Server/app.js
+++ Server/app.js
@@ -1,34 +1,16 @@
-const express = require("express");
-const connectDB = require("./db/conn");
+const express = require("express")
 const app = express();
+const router =require("./Routes/router")
 const PORT = 4000;
+const cors=require("cors");
 
+//Database connection
+const connectDB = require("./db/conn");
 connectDB();
-
-app.get("/", (req, res) => {
-    res.status(201).json("server started");
-});
-
+    
+app.use(express.json());
+app.use(cors());
+app.use(router);
 app.listen(PORT, () => {
     console.log(`Server running at port no ${PORT}`);
-});
-
-
-/*
-app.use(cors());
-app.use(express.json());
-app.use(router);
-*/
-//require("./db/conn");
-//const cors = require("cors");
-//const router = require("./Routes/router");
-//require("dotenv").config();
-
-
-//my change added
-//component 2
-
-//componet 3
-
-//component 4
-
+})

fe0eb051249f7773ce0b34ba1d53a05e13dd2444
Dipak Raj Giri <giridipak743@gmail.com>
2024-06-09 10:50:34 +0545

    signup 1

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,24 +2,27 @@ import React from 'react';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import Home from './Pages/Home/Home';
-import About_Us from './Pages/About_Us/About_Us';
+import AboutUs from './Pages/AboutUs/AboutUs';
 import Article from './Pages/Article/Article';
 import AI from './Pages/AI/AI';
 import Chat from './Pages/Chat/Chat';
-import Register from './Pages/Register/Register';
+import SignUp from './Pages/SignUp/SignUp';
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
-      <Routes>
+      
+        <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/about_us' element={<About_Us />} />
+        <Route path='/about_us' element={<AboutUs />} />
         <Route path='/article' element={<Article />} />
         <Route path='/ai' element={<AI />} />
         <Route path='/chat' element={<Chat />} />
-        <Route path='/register' element={<Register/>}/>
+        <Route path='/SignUp' element={<SignUp/>}/>
+       
+      
       </Routes>
       <Footer />
     </>
--- Server/Routes/router.js
+++ Server/Routes/router.js
@@ -1,14 +1,33 @@
 const express = require("express");
 const router = new express.Router();
-
+const userDB=require("../models/userSchema");
 router.get("/hello", (req, res) => {
     res.send("Hello World");
 });
 
-router.post("/register",async(req,res)=>{
-    console.log(req.body)
-    const{fname,lname,email,password}=req.body;
-    
-})
+router.post("/SignUp",async(req,res)=>{
+    try {
+        const { fname, lname, email, password } = req.body;
+        let user = await userDB.findOne({ email });
+        if (user) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+
+        user = new userDB({
+            fname,
+            lname,
+            email,
+            password
+        });
+
+        await user.save();
+        res.status(201).json({ message: 'Signup successful' });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+   
 
 module.exports=router;
+

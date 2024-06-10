64abcfdb1e96e57a0b88797793f6fa1852bf9b5a
DIPAJOSHI <joshideepa988@gmail.com>
2024-06-10 13:27:52 +0545

    footer added and images

--- Client/src/Components/Footer/Footer.js
+++ Client/src/Components/Footer/Footer.js
@@ -1,9 +1,22 @@
 import React from 'react'
-
-const Footer = () => {
+import './footer.css';
+import fb from '../../Assets/Images/fb.png';
+import linkedin from '../../Assets/Images/linkedin.png';
+import gmail from '../../Assets/Images/gmail.png';
+import github from '../../Assets/Images/github.png';
+function Footer (){
   return (
-    <div>
-      Footer
+    <div class="container_footer">
+      <div class="slogan">
+        <p>Urbar baari kisanko gahana<br/>
+          Swastha kheti hamro c.hahana</p>
+      </div>
+      <div class="images">
+   <img src={linkedin} alt="Linkedinimage"/>
+   <img src={fb} alt="Fbimage"/>
+   <img src={gmail} alt="gmailimage"/>
+   <img src={github} alt="githubimage"/></div>
+      <h4>&copy; 2024 copyright: Vecrosoft.com</h4>
     </div>
   )
 }

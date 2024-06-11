04515c2a46235f76f409cec1a3825542d030bd0f
DIPAJOSHI <joshideepa988@gmail.com>
2024-06-11 13:36:34 +0545

    chat box updated with new design

--- Client/src/App.js
+++ Client/src/App.js
@@ -7,9 +7,10 @@ import AboutUs from './Pages/AboutUs/AboutUs';
 import Article from './Pages/Article/Article';
 import AI from './Pages/AI/AI';
 import SignUp from './Pages/SignUp/SignUp';
+import Chat from './Pages/Chat/Chat'
 import SignIn from './Pages/SignIn/SignIn';
 import SideBar from './Components/SideBar/SideBar';
-import './App.css'; // Assuming you have an App.css file for global styles
+import './App.css';
 
 function App() {
   return (
@@ -24,12 +25,13 @@ function App() {
               <Route path='/about-us' element={<AboutUs />} />
               <Route path='/article' element={<Article />} />
               <Route path='/ai' element={<AI />} />
+              <Route path='/chat' element={<Chat />} />
               <Route path='/sign-up' element={<SignUp />} />
               <Route path='/sign-in' element={<SignIn />} />
             </Routes>
           </div>
         </div>
-        <Footer />
+        {/* <Footer /> */}
       </div>
    
   );

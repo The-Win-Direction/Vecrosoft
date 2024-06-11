a4d0a23cae77293e8a0679a8a1a6ef6e7d044181
DIPAJOSHI <joshideepa988@gmail.com>
2024-06-11 19:36:30 +0545

    description of project added with a image and added component to the ai page

---
+++ Client/src/Components/FeatureDescription/FeatureDescription.js
@@ -0,0 +1,23 @@
+// FeatureDescription.js
+import React from 'react';
+import './FeatureDescription.css';
+import featureImage from '../../../src/Assets/Images/featureImage.png';
+
+
+function FeatureDescription() {
+    return (
+      <div className="feature-description-container">
+        <div className="feature-text">
+          <h1>Detect Disease Automatically in 5 Seconds with One Click</h1>
+          <p>
+            Thanks to Vecrosoft's clever AI, you can slash detection time and have more fun! <br></br><br></br> Our advanced AI technology ensures that you can quickly and accurately detect plant diseases, giving you more time to enjoy your gardening and farming activities.
+          </p>
+        </div>
+        <div className="feature-image-container">
+          <img src={featureImage} alt="AI Feature" className="feature-image" />
+        </div>
+      </div>
+    );
+  }
+  
+  export default FeatureDescription;
--- Client/src/Pages/AI/AI.js
+++ Client/src/Pages/AI/AI.js
@@ -4,16 +4,20 @@ import './ai.css';
 import DiseaseDetection from '../../../src/Components/DiseaseDetection/DiseaseDetection';
 import DetectionDescription from '../../../src/Components/DetectionDescription/DetectionDescription';
 import SampleImages from '../../Components/SampleImages/SampleImages';
+import FeatureDescription from '../../Components/FeatureDescription/FeatureDescription';
 
 
 function AI() {
   return (
+    <div>
     <div className="ai-main-container">
     <DetectionDescription />
     <div className='detection-container-with-samples'>
       <DiseaseDetection />
       <SampleImages />
-    </div>
+    </div>  
+  </div>
+  <FeatureDescription/>
   </div>
   );
 }

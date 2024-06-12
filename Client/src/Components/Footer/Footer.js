a65b1a07a8dbb950b324a38a793e02d8fa11a816
DIPAJOSHI <joshideepa988@gmail.com>
2024-06-12 16:43:11 +0545

    footer updated with react icons and various links

--- Client/src/Components/Footer/Footer.js
+++ Client/src/Components/Footer/Footer.js
@@ -1,26 +1,31 @@
-import React from 'react'
+import React from 'react';
 import './footer.css';
-import fb from '../../Assets/Images/fb.png';
-import linkedin from '../../Assets/Images/linkedin.png';
-import gmail from '../../Assets/Images/gmail.png';
-import github from '../../Assets/Images/github.png';
-function Footer (){
-  return (<>
-    <div className='footer-margin-top'></div>
-    <div className="container_footer">
-      <div className="slogan">
-        <p>Urbar baari kisanko gahana<br/>
-          Swastha kheti hamro c.hahana</p>
+import { FaGithub, FaLinkedin, FaFacebook, FaEnvelope } from 'react-icons/fa'; // Importing icons from react-icons
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-left">
+        <h3>Urbar baari kisanko gahana<br/>
+        Swastha kheti hamro chahana</h3>
+        <p>&copy; 2024 Vecrosoft. All rights reserved.</p>
       </div>
-      <div className="images">
-   <img src={linkedin} alt="Linkedinimage"/>
-   <img src={fb} alt="Fbimage"/>
-   <img src={gmail} alt="gmailimage"/>
-   <img src={github} alt="githubimage"/></div>
-      <h4>&copy; 2024 copyright: Vecrosoft.com</h4>
-    </div>
-    </>
-  )
+      <div className="footer-right">
+        <div className="social-icons">
+          <a href="https://github.com/yourprofile" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+          <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+          <a href="https://facebook.com/yourprofile" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
+          <a href="mailto:your-email@example.com"><FaEnvelope /></a>
+        </div>
+        <div className="footer-links">
+          <a href="/terms-of-service">Terms of Service</a>
+          <a href="/general-terms-and-conditions">General Terms and Conditions</a>
+          <a href="/privacy-policy">Privacy Policy</a>
+          <a href="/cookie-policy">Cookie Policy</a>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer; 
+export default Footer;

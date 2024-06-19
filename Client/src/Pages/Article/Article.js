4af33f2c18c00184daba024aab5c83358de51a78
Dipa Joshi <joshideepa988@gmail.com>
2024-06-20 00:18:16 +0545

    article list styled and page made responsive

--- Client/src/Pages/Article/Article.js
+++ Client/src/Pages/Article/Article.js
@@ -3,15 +3,18 @@ import { Routes, Route } from 'react-router-dom';
 import ArticleList from '../../../src/Components/ArticleList/ArticleList';
 import FullArticle from '../../../src/Components/FullArticle/FullArticle';
 import Sidebar from '../../Components/SideBar/SideBar';
+import './Article.css';  // Make sure to import your CSS file
 
 const Article = () => {
   return (
     <div className='article-container'>
-       <Sidebar/>
-    <Routes>
-      <Route path="/" element={<ArticleList />} />
-      <Route path=":articleId" element={<FullArticle />} />
-    </Routes>
+      <div className='sidebar-container'>
+        <Sidebar />
+      </div>
+      <Routes>
+        <Route path="/" element={<ArticleList />} />
+        <Route path=":articleId" element={<FullArticle />} />
+      </Routes>
     </div> 
   ); 
 };

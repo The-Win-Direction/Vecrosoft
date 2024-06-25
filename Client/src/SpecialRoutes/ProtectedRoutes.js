2beef6424ec345175a4a8e968a482e8bb59f5e0c
Dipak Raj Giri <giridipak743@gmail.com>
2024-06-25 07:01:43 +0545

    protected routes implemented

---
+++ Client/src/SpecialRoutes/ProtectedRoutes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet, Navigate } from 'react-router-dom';
+import useAuth from '../Services/useAuth';
+
+const ProtectedRoute = () => {
+  const loading = useAuth();
+  const token = localStorage.getItem("userdatatoken");
+
+  if (loading) {
+    return <div>Loading...</div>; 
+  }
+
+  return token ? <Outlet /> : <Navigate to="/sign-in" />;
+};
+
+export default ProtectedRoute;

f8dd72fb7e2ab630c0959b9405410b86d402b22f
Dipa Joshi <joshideepa988@gmail.com>
2024-09-08 21:45:54 +0545

    ad page made by adding videos, and other  page designed

--- Client/src/Pages/Ads/Ads.js
+++ Client/src/Pages/Ads/Ads.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import SidebarDesktop from '../../Components/SidebarDesktop/SidebarDesktop';
-import sayariVideo from '../../Assets/Videos/video.mp4';
+import FWU_University1 from '../../Assets/Videos/FWU_University1.mp4';
+import FWU_University2 from '../../Assets/Videos/FWU_University2.mp4';
+import FWU_University3 from '../../Assets/Videos/FWU_University3.mp4';
 import './Ads.css';
 
 const Ads = () => {
@@ -8,10 +10,18 @@ const Ads = () => {
     <div className="ads-container">
       <SidebarDesktop />
       <div className="ads-content">
-        <h1>Advertisements</h1>
+        <h1>Advertisements </h1>
         <div className="video-container">
           <video controls>
-            <source src={sayariVideo} type="video/mp4" />
+            <source src={FWU_University1} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          <video controls>
+            <source src={FWU_University2} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          <video controls>
+            <source src={FWU_University3} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
